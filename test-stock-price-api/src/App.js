b30eb772eac84524e8e10b9d3cb8166ffbe6bbe2
Don86 <dh.teng.86@gmail.com>
2019-02-21 16:25:43 +1100

    pushed working chart in test-victory with chart.js

--- test-stock-price-api/src/App.js
+++ test-stock-price-api/src/App.js
@@ -5,7 +5,7 @@ function parseData(myInput) {
   // processes quandl data into a format for viz by victory
 
   // 1. Convert object of objects into array of objects
-  // create new key: date
+  // create new key: date (originally a key in the first level of objects)
   let newArray = []
   for (var key in myInput) {
       if (myInput.hasOwnProperty(key)) {
@@ -13,9 +13,16 @@ function parseData(myInput) {
           newArray.push(newRow)
       }
   }
-  // 2. Rename keys?
-  // Not implemented yet
-  return newArray
+  // 2. Generate plotData for chartjs2
+  let newArray2 = {labels: [] , datasets: [{data:[]}]}
+  for (var i = 0; i < newArray.length; i++) {
+    //console.log(newArray[i]["Date"], newArray[i]["4. close"])
+    newArray2["labels"].push(newArray[i]["Date"])
+    newArray2["datasets"][0]["data"].push(newArray[i]["4. close"])
+  }
+  console.log(newArray2)
+
+  return newArray2
 }
 
 class App extends Component {
@@ -26,15 +33,16 @@ class App extends Component {
 
   componentDidMount() {
     const api_key = "EEKL6B77HNZE6EB4"
-    fetch('https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=SPY&apikey=EEKL6B77HNZE6EB4')
+    fetch('https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=SPY&apikey='+api_key)
     .then(response => response.json())
     .then(data => parseData(data["Monthly Time Series"]))
-    .then(data => console.log(data))
   }
 
   render() {
     return (
-      <div></div>
+      <div>
+        Look at console.
+      </div>
     );
   }
 }
--- test-victory/src/App.js
+++ test-victory/src/App.js
@@ -1,55 +1,52 @@
 import React from 'react';
-import { VictoryChart, VictoryZoomContainer, VictoryLine, VictoryAxis, VictoryBrushContainer } from 'victory';
-import * as d3 from 'd3';
-import data from "./STI-small.csv"
-//import Moment from "moment"
-//import VicChart1 from "./components/VicChart1"
-// Modularization doesn't work atm
-// Still trying to figure out how to pass state to child components
+import { Line, Bar } from 'react-chartjs-2'
+//import data from "./STI-small.csv"
 
 function parseData(myInput) {
-  // processes quandl data into a format for viz by victory
+  // processes alpha vantage data into a format for viz by chart.js
 
   // 1. Convert object of objects into array of objects
-  // create new key: date
+  // create new key: date (originally a key in the first level of objects)
   let newArray = []
   for (var key in myInput) {
       if (myInput.hasOwnProperty(key)) {
-          const newRow = Object.assign({"newDate": new Date(key)}, {"Date":key}, {"Year":key.split("-")[0]}, myInput[key])
-          if (parseInt(newRow["Year"])>2016) {
-            newArray.push(newRow)
-          }
+          const newRow = Object.assign({"newDate": new Date(key)}, {"Date": key}, myInput[key])
+          newArray.push(newRow)
       }
   }
-  // 2. Rename keys?
-  // Not implemented yet
-  return newArray
+  //console.log(newArray)
+  // 2. Generate plotData for chartjs2
+  let newArray2 = {labels: [] , datasets: [{data:[]}]}
+  for (var i = 0; i < newArray.length; i++) {
+    newArray2["labels"].unshift(newArray[i]["Date"])
+    newArray2["datasets"][0]["data"].unshift(newArray[i]["4. close"])
+  }
+
+  // Reverse the arrays, because of the way that push works
+
+  // 3. Populate other fields of plotData for chartjs2
+  newArray2["datasets"][0]["label"] = "Stock chart (End-of-Month)"
+
+  return newArray2
 }
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      zoomDomain: {x: [new Date(2017, 1, 1), new Date(2019, 12, 1)]},
-      plotData: {}
+      zoomDomain: {x: [new Date(2000, 1, 1), new Date(2019, 12, 1)]},
+      plotData: {
+        labels: [],
+        datasets: [{
+          label: 'initialized label',
+          data: []
+        }]
+      }
     }
     this.handleZoom = this.handleZoom.bind(this)
   }
 
-  /*
-  componentDidMount() {
-    d3.csv(data).then(function(data) {
-      data.map(o => o.newDate = new Date(o.Date))
-      //console.log(data)
-    }).catch(function(err) {
-      throw err;
-    }).then(data =>{this.setState({plotData:data})})
-  }
-  */
-
-  
   // API call to alpha vantage
-  // console.log() for no; no setState()
   componentDidMount() {
     const ticker = "SPY"
     const api_key = "EEKL6B77HNZE6EB4"
@@ -59,7 +56,6 @@ class App extends React.Component {
     .then(data =>{this.setState({plotData:data})})
     .then(data => console.log(data))
   }
-  
 
   handleZoom(domain) {
     this.setState({ zoomDomain : domain })
@@ -78,47 +74,17 @@ class App extends React.Component {
       { newDate: new Date(2018, 12, 1), "4. close": 320 },
     ]
 
+    let localPlotData = this.state.plotData
+    console.log(localPlotData)
+
     return (
+      //Try React chart js2
       <div>
-        <VictoryChart width={600} height={200} scale={{x:"time"}}
-        containerComponent = {
-          <VictoryZoomContainer 
-          zoomDimension="x"
-          zoomDomain={this.state.zoomDomain}
-          onZoomDomainChange={this.handleZoom.bind(this)}
-        />}
-        >
-
-          <VictoryLine style={{ data: {stroke:"tomato"} }}
-            data={testData}
-            //data = {this.state.plotData}
-            x="newDate"
-            y="4. close"
-          />
-          </VictoryChart>
-        {/*<VicChart1 zoomDomain={this.state.zoomDomain}/>*/}
-
-        <VictoryChart
-          padding={{top:0, left:50, right:50, bottom:30}}
-          width={600} height={100} scale={{ x:"time" }}
-          containerComponent={
-            <VictoryBrushContainer 
-            brushDimension="x"
-            brushDomain={this.state.zoomDomain}
-            onBrushDomainChange={this.handleZoom.bind(this)}/>
-          }>
-          <VictoryAxis tickFormat={(x) => new Date(x).getFullYear()}
-          />
-          <VictoryLine
-          style={{data:{stroke:"tomato"}}}
-          data={testData}
-          //data = {this.state.plotData}
-          x="newDate"
-          y="4. close"
-          />
-        </VictoryChart>
+        <h1></h1>
+        <Line data={this.state.plotData} />
       </div>
-    )}
+    )
+  }
 }
 
 export default App;
